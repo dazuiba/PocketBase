d8565bbe8b398da44e05dfe64a97a00534fb1069
Sam Zhang <come2u@gmail.com>
2024-07-04 22:32:00 +0800

    rename to PocketBase

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "PBSwift",
+    name: "PocketBase",
     platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
 
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "PBSwift",
-            targets: ["PBSwift"]),
+            name: "PocketBase",
+            targets: ["PocketBase"]),
     ],
     dependencies: [
       .package(
@@ -25,9 +25,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PBSwift",dependencies:["AnyCodable"]),
+            name: "PocketBase",dependencies:["AnyCodable"]),
         .testTarget(
-            name: "PBSwiftTests",
-            dependencies: ["PBSwift","Quick","Nimble"]),
+            name: "PocketBaseTests",
+            dependencies: ["PocketBase","Quick","Nimble"]),
     ]
 )
